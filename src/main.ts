2d8cbbdb9cad04805fbc926e19d290a2a92a3f17
steve720zhang <steve720zhang@gmail.com>
2021-07-03 23:24:05 +0800

    add a intercaptor to reformat response data

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformerInterceptor } from './interceptor/transformer.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformerInterceptor())
   await app.listen(3000);
 }
 bootstrap();
