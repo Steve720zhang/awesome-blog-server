fcec2b619aa61d4e004d31bd78005222338f2d82
steve720zhang <steve720zhang@gmail.com>
2021-07-06 22:48:35 +0800

    fix typo

--- src/modules/article-tag/entities/article-tag-rec.entity.ts
+++ src/modules/article-tag/entities/article-tag-rec.entity.ts
@@ -1,12 +1,13 @@
 import { Entity, PrimaryGeneratedColumn, Column, PrimaryColumn } from "typeorm";
 
-@Entity(`article_tag_tec`)
+@Entity(`article_tag_rec`)
 export class ArticleTagRec {
-    @Column({ name: `article_id` })
-    articleId: number;
 
     @PrimaryColumn({ name: `article_tag_id` })
     articleTagId: number;
+
+    @Column({ name: `article_id` })
+    articleId: number;
 
     @Column({ name: `create_time` })
     createTime: Date;
