6e5f0e281071f9e139d2a4c666c706520794cfe2
zhangzequn <776021361@qq.com>
2021-07-19 19:55:18 +0800

    add auth to article modiles api

--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -1,35 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/services/auth/roles/roles.decorator';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { Roles as RolesEnum } from '../../services/auth/roles/roles.constant';
+import { AuthGuard as myAuthGuard } from '../../services/auth/auth.guard';
 
 @ApiTags('article')
 @Controller('article')
 export class ArticleController {
-  constructor(private readonly articleService: ArticleService) {}
+  constructor(private readonly articleService: ArticleService) { }
 
   @Post()
+  @Roles(RolesEnum.Member)
+  @UseGuards(myAuthGuard)
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articleService.create(createArticleDto);
   }
 
   @Get()
+  @Roles(RolesEnum.Visitor)
+  @UseGuards(myAuthGuard)
   findAll() {
     return this.articleService.findAll();
   }
 
   @Get(':id')
+  @Roles(RolesEnum.Visitor)
+  @UseGuards(myAuthGuard)
   findOne(@Param('id') id: string) {
     return this.articleService.findOne(+id);
   }
 
   @Patch(':id')
+  @Roles(RolesEnum.Member)
+  @UseGuards(myAuthGuard)
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articleService.update(+id, updateArticleDto);
   }
 
   @Delete(':id')
+  @Roles(RolesEnum.Member)
+  @UseGuards(myAuthGuard)
   remove(@Param('id') id: string) {
     return this.articleService.remove(+id);
   }
--- src/modules/article/article.module.ts
+++ src/modules/article/article.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
 import { Article } from './entities/article.entity';
+import { AuthModule } from 'src/services/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Article])],
+  imports: [TypeOrmModule.forFeature([Article]), AuthModule],
   controllers: [ArticleController],
   providers: [ArticleService]
 })
